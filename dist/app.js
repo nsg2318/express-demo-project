7c3e61f2bab8269518051dc3539cfaa81517c2b5
sunba <nsg2318@naver.com>
2022-06-27 18:50:46 +0900

    미들웨어 추가

--- dist/app.js
+++ dist/app.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
-var data = [1, 2, 3, 4];
-app.get('/', function (req, res) {
+app.use(function (req, res, next) {
     console.log(req.rawHeaders[1]);
+    console.log("this is middleware");
+    next();
+});
+app.get('/', function (req, res) {
     res.send(app_model_1.Cats);
 });
 app.get('/cats/blue', function (req, res) {
-    console.log(req.rawHeaders[1]);
     res.send(app_model_1.Cats[0]);
 });
 app.listen(8000, function () {
--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,18 @@ import {Cats, Cat} from './app.model';
 
 const app: express.Express = express();
 
-const data = [1,2,3,4];
+//미들 웨어 인터셉팅 같은 기능. Express는 위에서부터 찾으므로, app.use 가 중간 또는 아래에 있으면 그 위의 라우터에는 작동하지 않는다.
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log(`this is middleware`);
+  next();
+})
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send(Cats);
 })
 
 app.get('/cats/blue', (req, res) => {
-  console.log(req.rawHeaders[1]);
   res.send(Cats[0]);
 })
 
