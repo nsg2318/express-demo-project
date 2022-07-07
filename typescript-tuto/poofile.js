032bf5d2c60da1aaf1159897b69edd7af202c531
sunba <nsg2318@naver.com>
2022-07-07 20:10:27 +0900

    ts -> js 변환실습, typechecker 학습

---
+++ typescript-tuto/poofile.js
@@ -0,0 +1,3 @@
+var player;
+player = ["1", 2, true];
+console.log(player[0] === "2");
---
+++ typescript-tuto/poofile.ts
@@ -0,0 +1,5 @@
+let player: [string, number, boolean];
+
+player = ["1",2,true];
+
+console.log(player[0] === "2");
